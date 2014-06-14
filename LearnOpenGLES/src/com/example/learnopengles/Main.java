00cdd6287384e8f753dcf6769f1a1af7d5cdec6a
Alex Yang <gya10@sfu.ca>
2014-06-14 11:27:19 -0700

    Update

---
+++ LearnOpenGLES/src/com/example/learnopengles/Main.java
@@ -0,0 +1,38 @@
+package com.example.learnopengles;
+
+import android.app.Activity;
+import android.app.ActivityManager;
+import android.content.Context;
+import android.content.pm.ConfigurationInfo;
+import android.os.Bundle;
+import android.view.Menu;
+import android.widget.TextView;
+
+public class Main extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+		
+		final ActivityManager activityManager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
+		final ConfigurationInfo configurationInfo = activityManager.getDeviceConfigurationInfo();
+		final boolean supportsEs2 = configurationInfo.reqGlEsVersion >= 0x20000;
+		
+		TextView tv = (TextView) findViewById(R.id.textview1);
+		if(supportsEs2){
+			tv.setText("es2 is supported");
+		}
+		else{
+			tv.setText("es2 is not supported");
+		}
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+}
